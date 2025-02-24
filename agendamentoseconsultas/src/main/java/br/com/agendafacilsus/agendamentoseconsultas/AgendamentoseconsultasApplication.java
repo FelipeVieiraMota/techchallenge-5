719e96893df9ed9a8f541cc5f41acdcee6266190
felipevieiramota <felipevieiramota@gmail.com>
2025-02-24 23:09:29 +0000

    Second commit

---
+++ agendamentoseconsultas/src/main/java/br/com/agendafacilsus/agendamentoseconsultas/AgendamentoseconsultasApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendafacilsus.agendamentoseconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoseconsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoseconsultasApplication.class, args);
+	}
+
+}
---
+++ agendamentoseconsultas/src/test/java/br/com/agendafacilsus/agendamentoseconsultas/AgendamentoseconsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agendafacilsus.agendamentoseconsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoseconsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
