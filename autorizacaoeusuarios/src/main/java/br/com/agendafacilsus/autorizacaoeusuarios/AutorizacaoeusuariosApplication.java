c73468c56c587a9233405e62f0e3a0336c339073
felipevieiramota <felipevieiramota@gmail.com>
2025-02-24 23:03:23 +0000

    Added more content

---
+++ autorizacaoeusuarios/src/main/java/br/com/agendafacilsus/autorizacaoeusuarios/AutorizacaoeusuariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendafacilsus.autorizacaoeusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizacaoeusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorizacaoeusuariosApplication.class, args);
+	}
+
+}
