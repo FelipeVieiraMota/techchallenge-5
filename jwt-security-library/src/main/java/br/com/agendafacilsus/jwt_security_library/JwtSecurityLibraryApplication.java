26347f226c6e206a1efe14b6bcdd21d074d7c8e8
felipevieiramota <felipevieiramota@gmail.com>
2025-03-19 11:59:20 +0000

    Created security library

---
+++ jwt-security-library/src/main/java/br/com/agendafacilsus/jwt_security_library/JwtSecurityLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendafacilsus.jwt_security_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityLibraryApplication.class, args);
+	}
+
+}
---
+++ jwt-security-library/src/test/java/br/com/agendafacilsus/jwt_security_library/JwtSecurityLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agendafacilsus.jwt_security_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
