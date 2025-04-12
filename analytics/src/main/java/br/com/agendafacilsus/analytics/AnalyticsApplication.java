69a8ea50bb16b7f45e85530cb6ee2da1b079e880
felipevieiramota <felipevieiramota@gmail.com>
2025-04-12 18:07:01 +0100

    Created analytics project and added analytics dependencies inside the gateway project

---
+++ analytics/src/main/java/br/com/agendafacilsus/analytics/AnalyticsApplication.java
@@ -0,0 +1,15 @@
+package br.com.agendafacilsus.analytics;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticsApplication.class, args);
+	}
+
+}
